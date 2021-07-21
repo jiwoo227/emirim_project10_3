4727bcc01f7140d86c508ec903f066f90e82ed7e
최지우 <w2040@e-mirim.hs.kr>
2021-07-21 09:39:43 +0900

    10_3

--- app/src/main/java/com/example/emirim_project10_3/MainActivity.java
+++ app/src/main/java/com/example/emirim_project10_3/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.emirim_project10_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+ //       Button btncall = findViewById(Bundle savedInstanceState) {
+ //           super.onCreate(savedInstanceState);
+  //          setContentView(R.layout.activity_main);
+
+            Button btnCall = findViewById(R.id.btn_call);
+            Button btnFinish = findViewById(R.id.btn_done);
+
+            btnCall.setOnClickListener(btnListener);
+            btnFinish.setOnClickListener(btnListener);
+        }
+
+        View.OnClickListener btnListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                switch (v.getId()){
+                    case R.id.btn_call:
+                        Uri uri = Uri.parse("tel:01047954731");
+                        Intent intent = new Intent(Intent.ACTION_DIAL);
+                        startActivity(intent);
+                                break;
+                    case R.id.btn_done:
+                        break;
+                }
+            }
+        }
     }
 }
